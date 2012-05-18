9ac1cecff63913e2b84a141902702b10d6afcb86
Richie Zeng <richzeng@gmail.com>
2012-05-18 01:35:56 -0700

    Simplified the inplace_array example

--- ExampleDLM/ExampleDLM/ExampleDLM_complex.cpp
+++ ExampleDLM/ExampleDLM/ExampleDLM_complex.cpp
@@ -25,23 +25,16 @@ void IDL_CDECL inplace_array(int argc, IDL_VPTR argv[], char *argk)
   int *arr;
 
   IDL_ENSURE_ARRAY(argv[0]);
-  dim = argv[0]->value.arr->n_dim;
-  len = argv[0]->value.arr->n_elts;
+  dim = argv[0]->value.arr->n_dim; //dimensions
+  len = argv[0]->value.arr->n_elts; //number elements
 
-  //we need to convert the type elements of the array to IDL_LONG
-  //IDL_LONGs are the same as C ints
-  //the variable tmp is a pointer to the array of longs
-  IDL_VPTR tmp = IDL_CvtLng(1, &(argv[0]));
-
+  
   //grab the integer data from the tmp array
-  arr = (int *)tmp->value.arr->data;
+  arr = (short *)argv[0]->value.arr->data;
+  
+  //increment all the values
   for (int i=0; i < len; i++) {
     arr[i]++;
-  }
-  if (tmp != argv[0]) {
-    //converts the tmp array (now containing ints) back to IDL_VPTRs and copies
-    //it to the original argument passed in
-    IDL_VarCopy(tmp, argv[0]);
   }
 }
 
